2ba43a248e65eaefedc9b9b51e553b22fe96dffd
Chad Froebel <chad0989@rochester.rr.com>
2012-08-22 18:51:03 -0400

    Vendor update. Add some missing blobs and update adreno libs from qcom
    Change-Id: I20386568e107eef08dcc354174de2c97c67c629a

--- vigor-vendor-blobs.mk
+++ vigor-vendor-blobs.mk
@@ -23,7 +23,7 @@ PRODUCT_COPY_FILES += \
   vendor/htc/vigor/proprietary/lib/libril.so:obj/lib/libril.so \
   vendor/htc/vigor/proprietary/lib/libv8.so:obj/lib/libv8.so
 
-  
+
 PRODUCT_COPY_FILES += \
   vendor/htc/vigor/proprietary/BCM4330B1_002.001.003.0221.0228.hcd:system/vendor/firmware/BCM4330B1_002.001.003.0221.0228.hcd \
   vendor/htc/vigor/proprietary/fw_bcm4330_apsta.bin:system/vendor/firmware/fw_bcm4330_apsta.bin \
@@ -31,6 +31,7 @@ PRODUCT_COPY_FILES += \
   vendor/htc/vigor/proprietary/fw_bcm4330b2.bin:system/vendor/firmware/fw_bcm4330b2.bin \
   vendor/htc/vigor/proprietary/fw_bcm4330b2_apsta.bin:system/vendor/firmware/fw_bcm4330b2_apsta.bin \
   vendor/htc/vigor/proprietary/bin/bridgemgrd:system/bin/bridgemgrd \
+  vendor/htc/vigor/proprietary/bin/btld:system/bin/btld \
   vendor/htc/vigor/proprietary/bin/charging:system/bin/charging \
   vendor/htc/vigor/proprietary/bin/htc_ebdlogd:system/bin/htc_ebdlogd \
   vendor/htc/vigor/proprietary/bin/htcbatt:system/bin/htcbatt \
@@ -52,6 +53,9 @@ PRODUCT_COPY_FILES += \
   vendor/htc/vigor/proprietary/bin/gpsone_daemon:system/bin/gpsone_daemon \
   vendor/htc/vigor/proprietary/bin/port-bridge:system/bin/port-bridge \
   vendor/htc/vigor/proprietary/bin/snd3254:system/bin/snd3254 \
+  vendor/htc/vigor/proprietary/bin/awb_camera:system/bin/awb_camera \
+  vendor/htc/vigor/proprietary/bin/cam_ins_spmo:system/bin/cam_ins_spmo \
+  vendor/htc/vigor/proprietary/bin/lsc_camera:system/bin/lsc_camera \
   vendor/htc/vigor/proprietary/lib/libcryp98.so:system/lib/libcryp98.so \
   vendor/htc/vigor/proprietary/lib/libqdp.so:system/lib/libqdp.so \
   vendor/htc/vigor/proprietary/lib/libqmi.so:system/lib/libqmi.so \
@@ -112,18 +116,7 @@ PRODUCT_COPY_FILES += \
   vendor/htc/vigor/proprietary/etc/soundimage/srsfx_trumedia_music_wide.cfg:system/etc/soundimage/srsfx_trumedia_music_wide.cfg \
   vendor/htc/vigor/proprietary/etc/soundimage/srsfx_trumedia_music.cfg:system/etc/soundimage/srsfx_trumedia_music.cfg \
   vendor/htc/vigor/proprietary/etc/soundimage/srsfx_trumedia_voice.cfg:system/etc/soundimage/srsfx_trumedia_voice.cfg \
-  vendor/htc/vigor/proprietary/etc/firmware/leia_pfp_470.fw:system//etc/firmware/leia_pfp_470.fw \
-  vendor/htc/vigor/proprietary/etc/firmware/leia_pm4_470.fw:system//etc/firmware/leia_pm4_470.fw \
   vendor/htc/vigor/proprietary/etc/firmware/vidc_1080p.fw:system//etc/firmware/vidc_1080p.fw \
-  vendor/htc/vigor/proprietary/lib/egl/eglsubAndroid.so:system/lib/egl/eglsubAndroid.so \
-  vendor/htc/vigor/proprietary/lib/egl/libEGL_adreno200.so:system/lib/egl/libEGL_adreno200.so \
-  vendor/htc/vigor/proprietary/lib/egl/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
-  vendor/htc/vigor/proprietary/lib/egl/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
-  vendor/htc/vigor/proprietary/lib/egl/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
-  vendor/htc/vigor/proprietary/lib/libgsl.so:system/lib/libgsl.so \
-  vendor/htc/vigor/proprietary/lib/libC2D2.so:system/lib/libC2D2.so \
-  vendor/htc/vigor/proprietary/lib/libOpenVG.so:system/lib/libOpenVG.so \
-  vendor/htc/vigor/proprietary/lib/libsc-a2xx.so:system/lib/libsc-a2xx.so \
   vendor/htc/vigor/proprietary/lib/libaudioalsa.so:system/lib/libaudioalsa.so \
   vendor/htc/vigor/proprietary/lib/libaudcal.so:system/lib/libaudcal.so \
   vendor/htc/vigor/proprietary/lib/libacdbloader.so:system/lib/libacdbloader.so \
@@ -135,8 +128,6 @@ PRODUCT_COPY_FILES += \
   vendor/htc/vigor/proprietary/lib/libhtc_acoustic.so:system/lib/libhtc_acoustic.so \
   vendor/htc/vigor/proprietary/lib/libmmipl.so:system/lib/libmmipl.so \
   vendor/htc/vigor/proprietary/lib/libmmjpeg.so:system/lib/libmmjpeg.so \
-  vendor/htc/vigor/proprietary/lib/libmm-color-convertor.so:system/lib/libmm-color-convertor.so \
-  vendor/htc/vigor/proprietary/lib/libmpl.so:system/lib/libmpl.so \
   vendor/htc/vigor/proprietary/lib/liboemcamera.so:system/lib/liboemcamera.so \
   vendor/htc/vigor/proprietary/lib/libOlaEngine.so:system/lib/libOlaEngine.so \
   vendor/htc/vigor/proprietary/lib/libposteffect.so:system/lib/libposteffect.so \
@@ -146,7 +137,31 @@ PRODUCT_COPY_FILES += \
   vendor/htc/vigor/proprietary/lib/hw/lights.msm8660.so:system/lib/hw/lights.msm8660.so \
   vendor/htc/vigor/proprietary/lib/libmpl_jni.so:system/lib/libmpl_jni.so \
   vendor/htc/vigor/proprietary/lib/libmpl.so:system/lib/libmpl.so \
-  vendor/htc/vigor/proprietary/lib/libv8.so:system/lib/libv8.so \
   vendor/htc/vigor/proprietary/lib/libmllite.so:system/lib/libmllite.so \
-  vendor/htc/vigor/proprietary/lib/libmlplatform.so:system/lib/libmlplatform.so
+  vendor/htc/vigor/proprietary/lib/libmlplatform.so:system/lib/libmlplatform.so \
+  vendor/htc/vigor/proprietary/lib/libv8.so:system/lib/libv8.so
+
+# All the blobs necessary for adreno
+PRODUCT_COPY_FILES += \
+    vendor/htc/vigor/proprietary/etc/firmware/a225_pfp.fw:/system/etc/firmware/a225_pfp.fw \
+    vendor/htc/vigor/proprietary/etc/firmware/a225_pm4.fw:/system/etc/firmware/a225_pm4.fw \
+    vendor/htc/vigor/proprietary/etc/firmware/a225p5_pm4.fw:/system/etc/firmware/a225p5_pm4.fw \
+    vendor/htc/vigor/proprietary/etc/firmware/a300_pfp.fw:/system/etc/firmware/a300_pfp.fw \
+    vendor/htc/vigor/proprietary/etc/firmware/a300_pm4.fw:/system/etc/firmware/a300_pm4.fw \
+    vendor/htc/vigor/proprietary/etc/firmware/leia_pfp_470.fw:/system/etc/firmware/leia_pfp_470.fw \
+    vendor/htc/vigor/proprietary/etc/firmware/leia_pm4_470.fw:/system/etc/firmware/leia_pm4_470.fw \
+    vendor/htc/vigor/proprietary/etc/firmware/yamato_pfp.fw:/system/etc/firmware/yamato_pfp.fw \
+    vendor/htc/vigor/proprietary/etc/firmware/yamato_pm4.fw:/system/etc/firmware/yamato_pm4.fw \
+    vendor/htc/vigor/proprietary/lib/egl/egl.cfg:/system/lib/egl/egl.cfg \
+    vendor/htc/vigor/proprietary/lib/egl/eglsubAndroid.so:/system/lib/egl/eglsubAndroid.so \
+    vendor/htc/vigor/proprietary/lib/egl/libEGL_adreno200.so:/system/lib/egl/libEGL_adreno200.so \
+    vendor/htc/vigor/proprietary/lib/egl/libGLES_android.so:/system/lib/egl/libGLES_android.so \
+    vendor/htc/vigor/proprietary/lib/egl/libGLESv1_CM_adreno200.so:/system/lib/egl/libGLESv1_CM_adreno200.so \
+    vendor/htc/vigor/proprietary/lib/egl/libGLESv2_adreno200.so:/system/lib/egl/libGLESv2_adreno200.so \
+    vendor/htc/vigor/proprietary/lib/egl/libq3dtools_adreno200.so:/system/lib/egl/libq3dtools_adreno200.so \
+    vendor/htc/vigor/proprietary/lib/libC2D2.so:/system/lib/libC2D2.so \
+    vendor/htc/vigor/proprietary/lib/libgsl.so:/system/lib/libgsl.so \
+    vendor/htc/vigor/proprietary/lib/libOpenVG.so:/system/lib/libOpenVG.so \
+    vendor/htc/vigor/proprietary/lib/libsc-a2xx.so:/system/lib/libsc-a2xx.so
+
 
