1bdbe4d3349ec0687d8bbfba459bdbf03550a794
Chad Froebel <chad0989@rochester.rr.com>
2012-09-05 20:03:22 -0400

    Start attempting to fix bluetooth
    Change-Id: I522ecfc0df8759cab313d2642bc01e0f7169762f

--- vigor-vendor-blobs.mk
+++ vigor-vendor-blobs.mk
@@ -25,7 +25,7 @@ PRODUCT_COPY_FILES += \
 
 
 PRODUCT_COPY_FILES += \
-  vendor/htc/vigor/proprietary/etc/firmware/BCM4330B1_002.001.003.0550.0568.hcd:system/etc/firmware/BCM4330B1_002.001.003.0550.0568.hcd \
+  vendor/htc/vigor/proprietary/etc/firmware/BCM4330B1_002.001.003.0550.0568.hcd:system/etc/firmware/bcm4330B1.hcd \
   vendor/htc/vigor/proprietary/etc/firmware/fw_bcm4330_apsta_b1.bin:system/etc/firmware/fw_bcm4330_apsta_b1.bin \
   vendor/htc/vigor/proprietary/etc/firmware/fw_bcm4330_apsta_b2.bin:system/etc/firmware/fw_bcm4330_apsta_b2.bin \
   vendor/htc/vigor/proprietary/etc/firmware/fw_bcm4330_b1.bin:system/etc/firmware/fw_bcm4330_b1.bin \
@@ -33,7 +33,6 @@ PRODUCT_COPY_FILES += \
   vendor/htc/vigor/proprietary/etc/firmware/fw_bcm4330_p2p_b1.bin:system/etc/firmware/fw_bcm4330_p2p_b1.bin \
   vendor/htc/vigor/proprietary/etc/firmware/fw_bcm4330_p2p_b2.bin:system/etc/firmware/fw_bcm4330_p2p_b2.bin \
   vendor/htc/vigor/proprietary/bin/bridgemgrd:system/bin/bridgemgrd \
-  vendor/htc/vigor/proprietary/bin/btld:system/bin/btld \
   vendor/htc/vigor/proprietary/bin/charging:system/bin/charging \
   vendor/htc/vigor/proprietary/bin/htc_ebdlogd:system/bin/htc_ebdlogd \
   vendor/htc/vigor/proprietary/bin/htcbatt:system/bin/htcbatt \
